1bf9ec1e8ddcb046dff92384f2afe6e2d66a1b75
Trevor <trevorj342@gmail.com>
2021-08-02 22:29:13 -0400

    i believe i fixed audioreplacer

--- AudioReplacer/MelonLoaderMod.cs
+++ AudioReplacer/MelonLoaderMod.cs
@@ -13,7 +13,7 @@ namespace AudioReplacer
         public const string Name = "AudioReplacer";
         public const string Author = "trev";
         public const string Company = null;
-        public const string Version = "1.1.1";
+        public const string Version = "1.2.0";
         public const string DownloadLink = null;
     }
 
@@ -22,26 +22,30 @@ namespace AudioReplacer
         public static Dictionary<string, AudioClip> AudioClips = new Dictionary<string, AudioClip>();
         public static bool LogSounds;
 
-        private string pathToSongs = Directory.GetCurrentDirectory() + "\\UserData\\CustomAudio\\";
+        private string customAudioPath = Path.Combine(MelonUtils.UserDataDirectory, "CustomAudio");
+
+        private string[] allowedExts = new string[] { ".wav", ".mp3" };
 
         public override void OnApplicationStart()
         {
             BassImporter reader = new BassImporter();
-            if (!Directory.Exists(pathToSongs))
-                Directory.CreateDirectory(pathToSongs);
+            if (!Directory.Exists(customAudioPath))
+                Directory.CreateDirectory(customAudioPath);
 
             var category = MelonPreferences.CreateCategory("AudioReplacer", "");
             category.CreateEntry("LogSounds", false);
             LogSounds = category.GetEntry<bool>("LogSounds").Value;
 
-            string[] wavs = Directory.GetFiles(pathToSongs, "*.wav *.mp3");
-            for (int i = 0; i < wavs.Length; i++)
+            string[] audioFiles = Directory.GetFiles(customAudioPath, "*", SearchOption.AllDirectories);
+            for (int i = 0; i < audioFiles.Length; i++)
             {
-                reader.Import(wavs[i]);
+                if (!allowedExts.Contains(Path.GetExtension(audioFiles[i]))) continue;
+
+                reader.Import(audioFiles[i]);
 
                 if (reader.audioClip == null)
                 {
-                    MelonLogger.Error("Failed to import " + Path.GetFileName(wavs[i]));
+                    MelonLogger.Error("Failed to import " + Path.GetFileName(audioFiles[i]));
                     continue;
                 }
                 reader.audioClip.hideFlags = HideFlags.DontUnloadUnusedAsset;
