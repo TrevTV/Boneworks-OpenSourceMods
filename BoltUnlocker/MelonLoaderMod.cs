2c4da1778fe7126bac0ef0c9a2576cba4567153c
Trevor <trevorj342@gmail.com>
2021-11-11 17:46:21 -0500

    push boltunlocker

---
+++ BoltUnlocker/MelonLoaderMod.cs
@@ -0,0 +1,76 @@
+﻿using MelonLoader;
+using ModThatIsNotMod;
+using StressLevelZero.Pool;
+using StressLevelZero.Props.Weapons;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoltUnlocker
+{
+    public static class BuildInfo
+    {
+        public const string Name = "BoltUnlocker";
+        public const string Author = "trev";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    // this code is the opposite of professional but i dont give a fuck
+    public class Core : MelonMod
+    {
+        public static MelonPreferences_Entry<string[]> doShitTo;
+        public static List<string> poolsToDoShitTo;
+
+        public override void OnApplicationStart()
+        {
+            MelonPreferences_Category category = MelonPreferences.CreateCategory("BoltUnlocker");
+            doShitTo = category.CreateEntry("DoTheThingTo", new string[] { "Gun names here", "CaSe SeNsItIvE" });
+            poolsToDoShitTo = new List<string>();
+            HarmonyInstance.Patch(typeof(Gun).GetMethod("CompleteSlidePull"), null, typeof(Core).GetMethod(nameof(OnCompleteSlidePull)).ToNewHarmonyMethod());
+            Hooking.OnGrabObject += Hooking_OnGrabObject;
+        }
+
+        private void Hooking_OnGrabObject(UnityEngine.GameObject obj, StressLevelZero.Interaction.Hand hand)
+        {
+            Poolee poolee = obj.GetComponentInParent<Poolee>();
+            if (poolee == null)
+            {
+                //MelonLogger.Msg(obj.name + " has no Poolee");
+                return;
+            }
+
+            if (poolee != null && !poolsToDoShitTo.Contains(poolee.pool.Prefab.name))
+            {
+                MelonLogger.Msg("checking pool of prefab: " + poolee.pool.Prefab.name);
+                if (doShitTo.Value.Contains(poolee.pool.Prefab.name))
+                    poolsToDoShitTo.Add(poolee.pool.Prefab.name);
+            }
+        }
+
+        public static void AfterGunStart(Gun __instance)
+        {
+            Poolee poolee = __instance.GetComponent<Poolee>();
+            if (poolee == null) MelonLogger.Msg(__instance.name + " has no Poolee");
+
+            if (poolee != null && !poolsToDoShitTo.Contains(poolee.pool.Prefab.name))
+            {
+                MelonLogger.Msg("checking pool of prefab: " + poolee.pool.Prefab.name);
+                if (doShitTo.Value.Contains(poolee.pool.Prefab.name))
+                    poolsToDoShitTo.Add(poolee.pool.Prefab.name);
+            }
+        }
+
+        public static void OnCompleteSlidePull(Gun __instance)
+        {
+            //MelonLogger.Msg("hi this fucker just did a thing: " + __instance.name);
+            if (poolsToDoShitTo.Contains(SimpleHelpers.GetCleanObjectName(__instance.name))
+                && __instance.chamberedCartridge == null && !__instance._isSlideGrabbed)
+            {
+                //MelonLogger.Msg("doing the thing :thumbsup:");
+                __instance.isFiring = false;
+                __instance.SlideRelease();
+            }
+        }
+    }
+}
---
+++ BoltUnlocker/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(BoltUnlocker.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(BoltUnlocker.BuildInfo.Company)]
+[assembly: AssemblyProduct(BoltUnlocker.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BoltUnlocker.BuildInfo.Author)]
+[assembly: AssemblyTrademark(BoltUnlocker.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(BoltUnlocker.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BoltUnlocker.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(BoltUnlocker.Core), BoltUnlocker.BuildInfo.Name, BoltUnlocker.BuildInfo.Version, BoltUnlocker.BuildInfo.Author, BoltUnlocker.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
